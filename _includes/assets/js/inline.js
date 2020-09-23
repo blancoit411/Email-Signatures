a1a03e116f9803eda9ccd1170c8a55b9f119611e
Luke Bishop <42811624+lukebishop121@users.noreply.github.com>
2020-09-23 12:35:23 -0500

    Mobile Navigation Part 1

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -8,15 +8,42 @@ if (window.netlifyIdentity) {
   });
 }
 
+
 function highlightDate() {
   let days= ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
   let currentDay = days[new Date().getDay()];
   let selected = document.getElementById(currentDay);
   selected.classList.add("currentDay")
+
 }
 
-// function getCurrentYear() {
-//   let currentYear = new Date().getFullYear();
-//   let selected = document.getElementById("currentYear");
-//   selected.innerHTML = currentYear;
-// }
+function navToggleEvent() {
+  let menuState = getComputedStyle(document.getElementById('navLinks')).opacity;
+
+  if (menuState == '1')
+  {
+    navClose();
+    return;
+  }
+  
+  if (menuState == '0')
+  {
+    navOpen();
+    return;
+  }
+}
+
+
+function navOpen()
+{
+  document.getElementById("navLinks").classList.remove('hidden');
+  document.getElementById("navToggle").classList.add('rotated');
+  return;
+}
+
+function navClose()
+{
+  document.getElementById("navLinks").classList.add('hidden');
+  document.getElementById("navToggle").classList.remove('rotated');
+  return;
+}
